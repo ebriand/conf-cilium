0a485898e29fb2f1671ce0e2667d34c50a0d4f77
Eric Briand <eric.briand@gmail.com>
2019-04-02 16:42:48 +0200

    :tada: api implemented$

--- api/main.go
+++ api/main.go
@@ -1,19 +1,100 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "github.com/gorilla/mux"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprint(w, "Hello")
+type Hero struct {
+	Name             string `json:"name"`
+	SecretIdentityID int    `json:"secretIdentityID"`
+}
+
+type Identity struct {
+	ID       int    `json:"id"`
+	RealName string `json:"realName"`
+}
+
+var heroes = []Hero{
+	{"batman", 1},
+	{"superman", 2},
+}
+
+var identities = []Identity{
+	{1, "Bruce Wayne"},
+	{2, "Kalel"},
+}
+
+func heroesToNames(heroes []Hero) []string {
+	var names = []string{}
+	for _, h := range heroes {
+		names = append(names, h.Name)
+	}
+	return names
+}
+
+func getHeroByName(name string) (*Hero, error) {
+	for _, h := range heroes {
+		if name == h.Name {
+			return &h, nil
+		}
+	}
+	return nil, fmt.Errorf("hero %s not found", name)
+}
+
+func getIdentityByID(id int) (*Identity, error) {
+	for _, i := range identities {
+		if id == i.ID {
+			return &i, nil
+		}
+	}
+	return nil, fmt.Errorf("identity %d not found", id)
+}
+
+func HeroesHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(heroesToNames(heroes))
+}
+
+func HeroHandler(w http.ResponseWriter, r *http.Request) {
+	name := mux.Vars(r)["name"]
+	hero, err := getHeroByName(name)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(hero)
+}
+
+func IdentitiesHandler(w http.ResponseWriter, r *http.Request) {
+	idString := mux.Vars(r)["identityID"]
+	id, err := strconv.Atoi(idString)
+	if err != nil {
+		fmt.Printf("Identity id %s not an integer", idString)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	identity, err := getIdentityByID(id)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(identity)
 }
 
 func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/", HomeHandler)
-    http.Handle("/", r)
-    http.ListenAndServe(":80", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/heroes", HeroesHandler).Methods("GET")
+	r.HandleFunc("/heroes/{name}", HeroHandler).Methods("GET")
+	r.HandleFunc("/identities/{identityID:[0-9]+}", IdentitiesHandler).Methods("GET")
+	http.Handle("/", r)
+	http.ListenAndServe(":80", nil)
 }
