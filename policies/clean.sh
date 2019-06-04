392ce75d065718386ccb6bc4bb8eeeac49d4828f
Eric Briand <eric.briand@gmail.com>
2019-06-04 10:01:21 +0200

    :bug: fix cleaning script

--- policies/clean.sh
+++ policies/clean.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
-kubectl delete cnp --all --all-namespaces
-kubectl delete netpol --all --all-namespaces
+kubectl delete cnp --all -n kafka
+kubectl delete cnp --all -n api
+kubectl delete netpol --all -n internal
